301fa094569c048e3b69ff003c0ca825dbb6b95e
matt.turi <matt.turi@modalai.com>
2022-02-07 11:34:24 -0800

    updated build flags for new cross compilers

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ TOOLCHAIN64="/opt/cross_toolchain/aarch64-gnu-4.9.toolchain.cmake"
 
 mkdir -p build64
 cd build64
-cmake -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN64} ../
+cmake -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN64} -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -std=c++11 -march=armv8-a -L  /usr/aarch64-linux-gnu-2.23/lib -I  /usr/aarch64-linux-gnu-2.23/include" ../
 make -j4
 cd ../
