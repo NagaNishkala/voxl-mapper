b0c856e332017b96f09bab79bb2b253080842257
mattbooker <mattbooker97@gmail.com>
2022-07-17 12:59:23 -0700

    Added a local planner that only smoothes the global plan

---
+++ server/voxl-mapper/local_planners/global_smoother.cc
@@ -0,0 +1,52 @@
+#include "global_smoother.h"
+
+GlobalSmoother::GlobalSmoother(voxblox::TsdfServer *map, int plan_ch, int render_ch)
+    : map_(map),
+      plan_ch_(plan_ch),
+      render_ch_(render_ch)
+{
+}
+
+void GlobalSmoother::setup()
+{
+    setupSmootherFromConfig(loco_smoother_, map_->getEsdfMapPtr().get());
+}
+
+void GlobalSmoother::setPlan(const Point3fVector &waypoints)
+{
+    // Copy the waypoints
+    waypoints_ = Point3fVector(waypoints);
+}
+
+void GlobalSmoother::start()
+{
+    mav_msgs::EigenTrajectoryPointVector waypoints_for_smoother;
+    convertPointsToSmootherFormat(waypoints_, waypoints_for_smoother);
+
+    mav_trajectory_generation::Trajectory trajectory;
+
+    if (loco_smoother_.getTrajectoryBetweenWaypoints(waypoints_for_smoother, &trajectory))
+    {
+        int segment_id = 0;
+
+        trajectory_t out;
+
+        if (!convertMavTrajectoryToVoxlTrajectory(trajectory, out))
+            return false;
+
+        out.magic_number = TRAJECTORY_MAGIC_NUMBER;
+
+        // Set segment id for each segment whilst incrementing id
+        for (int i = 0; i < out.n_segments; i++)
+            out.segments[i].id = segment_id++;
+
+        out.traj_command = TRAJ_CMD_LOAD_AND_START;
+
+        printf("Sending trajectory to plan channel\n");
+        pipe_server_write(plan_ch_, &out, sizeof(trajectory_t));
+    }
+    else
+    {
+        printf("Smoother failed to find a path.\n");
+    }
+}
---
+++ server/voxl-mapper/local_planners/global_smoother.h
@@ -0,0 +1,26 @@
+#ifndef GLOBAL_SMOOTHER_H
+#define GLOBAL_SMOOTHER_H
+
+#include <mav_path_smoothing/loco_smoother.h>
+#include "voxl_mapper.h"
+#include "local_planner.h"
+
+class GlobalSmoother : public LocalPlanner
+{
+    public:
+    GlobalSmoother(voxblox::TsdfServer *map, int plan_ch, int render_ch);
+    ~GlobalSmoother();
+
+    void setPlan(const Point3fVector &waypoints);
+    void setup();
+    void start();
+    void stop();
+
+    private:
+    Point3fVector waypoints_;
+    mav_planning::LocoSmoother loco_smoother_;
+    voxblox::TsdfServer* map_;
+
+    int plan_ch_;
+    int render_ch_;
+};
