918e6e5854e474959d809624e5e9a0d21f8e2e9f
Matthew Booker <matthew.booker@modalai.com>
2022-07-18 16:41:16 -0700

    Make sure only one instance of mapper is running

--- server/voxl-mapper/main.cc
+++ server/voxl-mapper/main.cc
@@ -2,19 +2,20 @@
 #include "config_file.h"
 #include <unistd.h>
 #include <getopt.h>
-#include <sys/stat.h>	// for mkdir
-#include <sys/types.h>	// for mode_t in mkdir
+#include <sys/stat.h>  // for mkdir
+#include <sys/types.h> // for mode_t in mkdir
 #include <fstream>
 #include <iostream>
 
+#define PROCESS_NAME "voxl-mapper"
 
-bool en_debug= false;
-bool en_timing= false;
+bool en_debug = false;
+bool en_timing = false;
 
-inline bool exists_ (const char *name)
+inline bool exists_(const char *name)
 {
-    std::ifstream f(name);
-    return f.good();
+	std::ifstream f(name);
+	return f.good();
 }
 
 static int _mkdir(const char *dir)
@@ -40,8 +41,9 @@ static int _mkdir(const char *dir)
 	return 0;
 }
 
-static void _print_usage(void){
-    	printf("\n\
+static void _print_usage(void)
+{
+	printf("\n\
 voxl-mapper usually runs as a systemd background service. However, for debug\n\
 purposes it can be started from the command line manually with any of the following\n\
 debug options. When started from the command line, voxl-mapper will automatically\n\
@@ -55,27 +57,31 @@ stop the background service so you don't have to stop it manually\n\
 	return;
 }
 
-static int _parse_opts(int argc, char* argv[])
+static int _parse_opts(int argc, char *argv[])
 {
 	static struct option long_options[] =
-	{
-        {"config",				    no_argument,		    0, 'c'},
-        {"debug",					no_argument,		    0, 'd'},
-        {"timing",					no_argument,		    0, 't'},
-        {"help",					no_argument,		    0, 'h'},
-	};
+		{
+			{"config", no_argument, 0, 'c'},
+			{"debug", no_argument, 0, 'd'},
+			{"timing", no_argument, 0, 't'},
+			{"help", no_argument, 0, 'h'},
+		};
 
-	while(1){
+	while (1)
+	{
 		int option_index = 0;
 		int c = getopt_long(argc, argv, "cm:i:s:dthz", long_options, &option_index);
 
-		if(c == -1) break; // Detect the end of the options.
+		if (c == -1)
+			break; // Detect the end of the options.
 
-		switch(c){
+		switch (c)
+		{
 		case 0:
 			// for long args without short equivalent that just set a flag
 			// nothing left to do so just break.
-			if (long_options[option_index].flag != 0) break;
+			if (long_options[option_index].flag != 0)
+				break;
 			break;
 
 		case 'c':
@@ -83,12 +89,12 @@ static int _parse_opts(int argc, char* argv[])
 			exit(0);
 			break;
 
-        case 'd':
-            en_debug = true;
+		case 'd':
+			en_debug = true;
 			break;
 
-        case 't':
-            en_timing = true;
+		case 't':
+			en_timing = true;
 			break;
 
 		case 'h':
@@ -103,63 +109,92 @@ static int _parse_opts(int argc, char* argv[])
 	return 0;
 }
 
-int main(int argc, char** argv) {
-    if(_parse_opts(argc, argv)) return -1;
+int main(int argc, char **argv)
+{
+	if (_parse_opts(argc, argv))
+		return -1;
 
 	printf("Loading our own config file\n");
-    if(config_file_read()) return -1;
-    if (en_debug) config_file_print();
+	if (config_file_read())
+		return -1;
+	if (en_debug)
+		config_file_print();
 
-    printf("Loading extrinsics config file\n");
-	if(load_extrinsics_file(en_debug)) return -1;
+	printf("Loading extrinsics config file\n");
+	if (load_extrinsics_file(en_debug))
+		return -1;
+
+	/* make sure another instance isn't running
+	 * if return value is -3 then a background process is running with
+	 * higher privaledges and we couldn't kill it, in which case we should
+	 * not continue or there may be hardware conflicts. If it returned -4
+	 * then there was an invalid argument that needs to be fixed.
+	 */
+	if (kill_existing_process(PROCESS_NAME, 2.0) < -2)
+	{
+		std::cerr << "ERROR: could not kill existing process" << std::endl;
+		exit(-1);
+	}
+
+	/* make PID file to indicate your project is running
+	 * due to the check made on the call to rc_kill_existing_process() above
+	 * we can be fairly confident there is no PID file already and we can
+	 * make our own safely.
+	 */
+	make_pid_file(PROCESS_NAME);
 
     _mkdir("/data/voxl_mapper/");
 
-    if (!exists_(esdf_save_path)){
-        std::ofstream outfile (esdf_save_path);
-        outfile.close();
-    }
-    if (!exists_(tsdf_save_path)){
-        std::ofstream outfile (tsdf_save_path);
-        outfile.close();
-    }
-    if (!exists_(mesh_save_path)){
-        std::ofstream outfile (mesh_save_path);
-        outfile.close();
-    }
-    // setup tsdf configs
-    voxblox::TsdfMap::Config map_config;
-    voxblox::TsdfIntegratorBase::Config int_config;
-    map_config.tsdf_voxel_size = voxel_size;
-    map_config.tsdf_voxels_per_side = voxels_per_side;
+	if (!exists_(esdf_save_path))
+	{
+		std::ofstream outfile(esdf_save_path);
+		outfile.close();
+	}
+	if (!exists_(tsdf_save_path))
+	{
+		std::ofstream outfile(tsdf_save_path);
+		outfile.close();
+	}
+	if (!exists_(mesh_save_path))
+	{
+		std::ofstream outfile(mesh_save_path);
+		outfile.close();
+	}
+	// setup tsdf configs
+	voxblox::TsdfMap::Config map_config;
+	voxblox::TsdfIntegratorBase::Config int_config;
+	map_config.tsdf_voxel_size = voxel_size;
+	map_config.tsdf_voxels_per_side = voxels_per_side;
 
-    if(enable_signal_handler()==-1){
-		fprintf(stderr,"ERROR: failed to start signal handler\n");
+	if (enable_signal_handler() == -1)
+	{
+		fprintf(stderr, "ERROR: failed to start signal handler\n");
 		return -1;
 	}
 
-    voxblox::MeshIntegratorConfig mesh_config;
+	voxblox::MeshIntegratorConfig mesh_config;
 
-    printf("Trying to init tsdf server\n");
-    // initialize tsdf server
-    voxblox::TsdfServer tsdf_server(map_config, int_config, mesh_config, en_debug, en_timing);
-    printf("created tsdf server\n");
+	printf("Trying to init tsdf server\n");
+	// initialize tsdf server
+	voxblox::TsdfServer tsdf_server(map_config, int_config, mesh_config, en_debug, en_timing);
+	printf("created tsdf server\n");
 	// indicate to the soon-to-be-started thread that we are initialized
 	// and running, this is an extern variable in start_stop.c
-    main_running = 1;
+	main_running = 1;
 
-    //start recieving and integrating data
-    tsdf_server.initMPA();
+	// start recieving and integrating data
+	tsdf_server.initMPA();
 
 	////////////////////////////////////////////////////////////////////////////////
 	// main loop just waits and keeps trying to open pipes if they are closed
 	////////////////////////////////////////////////////////////////////////////////
-    while(main_running){
-        usleep(500000);
-    }
-    ////////////////////////////////////////////////////////////////////////////////
+	while (main_running)
+	{
+		usleep(500000);
+	}
+	////////////////////////////////////////////////////////////////////////////////
 	// close everything
 	////////////////////////////////////////////////////////////////////////////////
-    tsdf_server.closeMPA();
-    return 0;
+	tsdf_server.closeMPA();
+	return 0;
 }
